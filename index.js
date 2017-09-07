190c5edb88458818101c51fdf2b80e27769b78eb
Olena Ageyeva <eageeva@hotmail.com>
2017-09-07 16:59:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function addToCart(itemName) {
  var newItem=new Object()
  newItem={ [itemName]:itemPrice }
  console.log(`${itemName} has been added to your cart.`)
- cart.push(newItem)
+ cart=[...cart, newItem]
  return (cart)
 }
 

2a8baa69ae0690a116184c76f46aa4018dcd1de1
Olena Ageyeva <eageeva@hotmail.com>
2017-09-07 16:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function setCart(c) {
 function addToCart(itemName) {
  // write your code here
  var itemPrice=Math.random()*(100-1)+1
- var newItem=new Object({ [itemName]:itemPrice })
+ var newItem=new Object()
+ newItem={ [itemName]:itemPrice }
  console.log(`${itemName} has been added to your cart.`)
  cart.push(newItem)
  return (cart)
